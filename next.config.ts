86a0cdc7c04c0dd5d5c4ca16f100747597786ddb
gouraryan090-rgb <gouraryan090@gmail.com>
2026-08-22 13:25:03 +0530

    Updated code

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/howtopenmyadminpanel7",
+        destination: "/admin",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
